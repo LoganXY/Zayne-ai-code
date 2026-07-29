2cbffe8af65a6e00b5f0320eaabe7f66c40456e5
Zayne <447604064@qq.com>
2026-07-29 14:51:06 +0800

    十、AI 工作流（LangGraph4j 高级特性实战 - 并发）
    1. 基于 LangGragh4j Parallel Branch 实现并发

--- src/main/java/com/zayne/zayneaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/zayne/zayneaicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.zayne.zayneaicodemother.langgraph4j.state;
 
+import com.zayne.zayneaicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.zayne.zayneaicodemother.langgraph4j.model.QualityResult;
 import com.zayne.zayneaicodemother.model.enums.CodeGenTypeEnum;
 import lombok.AllArgsConstructor;
@@ -77,6 +78,21 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
+
 
 
     @Serial
